ce111697678a3ddde229d7179328d1148aab7ced
CuzIm_ZD <lolmhtrf14@gmail.com>
2024-09-16 03:47:30 +0300

    gg

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 document.getElementById('dateForm').addEventListener('submit', function(event) {
     event.preventDefault();
     const birthDateInput = document.getElementById('birthDate').value;
+
     if (birthDateInput) {
+
         const birthDate = new Date(birthDateInput);
         const today = new Date();
         const timeDifference = today - birthDate;
@@ -10,9 +12,10 @@ document.getElementById('dateForm').addEventListener('submit', function(event) {
         const daysLived = Math.floor(timeDifference / (1000 * 60 * 60 * 24)); 
         document.getElementById('resultAge').textContent = `You are ${Age} years old.`;
         document.getElementById('resultDays').textContent = `You have lived for: ${daysLived} Days.`;
-        document.getElementById('resultHours').textContent = ` And ${hoursLived} Hours.`;
-
+        document.getElementById('resultHours').textContent = `And ${hoursLived} Hours.`;
     } else {
         document.getElementById('resultAge').textContent = 'Please enter a valid date.';
+        document.getElementById('resultDays').textContent = ``;
+        document.getElementById('resultHours').textContent = ``;
     }
 });
